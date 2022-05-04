579a9d1aa0e0e9c48d51f61ea877e950a339a86e
la55u <andras9405@gmail.com>
2022-05-04 11:35:57 +0200

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  staticPageGenerationTimeout: 10,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
